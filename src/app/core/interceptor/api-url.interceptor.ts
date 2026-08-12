240ca116f44f3140e42658644311bbb6387138fd
Raimundos Marques <devraimundos@outlook.com>
2026-08-12 20:26:04 -0300

    feat: implementa interceptador pra inserir o baseUrl em toda request para o back-end

---
+++ src/app/core/interceptor/api-url.interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+export const ApiUrlInterceptor: HttpInterceptorFn = (request, next) => {
+    if (request.url.startsWith('/')) {
+        const apiRequest = request.clone({ url: `${environment.apiUrl}${request.url}` });
+        return next(apiRequest);
+    }
+    return next(request);
+};
