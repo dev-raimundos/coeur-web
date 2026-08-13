7de5b16f0fdbd5a8f569c5579ac70954e0dce818
dev-raimundos <devraimundos@outlook.com>
2026-08-13 11:34:21 -0300

    feat: adiciona telas de not-found e internal-error

---
+++ src/app/core/screens/internal-error/internal-error.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-internal-error',
+    imports: [],
+    templateUrl: './internal-error.component.html',
+    styleUrl: './internal-error.component.scss',
+})
+export class InternalErrorComponent {}
